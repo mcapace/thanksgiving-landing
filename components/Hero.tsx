917f2bda781b821653090866cb8ef37de5870e14
mcapace <mike.capace@gmail.com>
2025-10-08 17:13:31 -0400

    Increase hero image height to show full image
    HERO IMAGE SIZE:
    - Increased from 70vh to 75vh
    - Min height: 500px → 550px
    - Max height: 700px → 800px
    
    RESULT:
    ✨ Taller hero section
    ✨ Full image accommodated
    ✨ Better visual impact
    ✨ More of turkey/platter visible

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { ChevronDown } from "lucide-react";
 
 export default function Hero() {
   return (
-    <section className="relative h-[70vh] min-h-[500px] max-h-[700px] w-full overflow-hidden">
+    <section className="relative h-[75vh] min-h-[550px] max-h-[800px] w-full overflow-hidden">
       {/* Background Image */}
       <div className="absolute inset-0">
         <Image
