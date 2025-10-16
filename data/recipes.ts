fedb93a425659e61df41de8667dd9ee18c2a3868
mcapace <mike.capace@gmail.com>
2025-10-16 13:10:59 -0400

    Update St. Supéry to use new StSuperyvNEW.pdf file

--- data/recipes.ts
+++ data/recipes.ts
@@ -124,7 +124,7 @@ export const recipes: Recipe[] = [
     recipePath: "/images/recipes-food/St Supery Recipe.jpg",
     category: "side",
     wineType: "Other",
-    pdfPath: "/recipes/StSuperyv2.pdf"
+    pdfPath: "/recipes/StSuperyvNEW.pdf"
   },
   {
     id: 9,
